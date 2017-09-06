35404108e04b9ba83b8a3372de51a6e130e2b518
pdvega <pdvegaa@gmail.com>
2017-09-07 01:47:25 +0700

    kanban done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,28 +2,30 @@
   <div class="container">
     <div class="row">
       <div class="col-md-12">
-        <h1>KANBAN VUEFIRE</h1>
+        <h1>KANBAN VUEFIRE BOARD</h1>
         <br>
-        <button class="btn btn-primary" data-toggle="modal" data-target="#myModal">Add New Task</button>
+        <button class="btn btn-primary" data-toggle="modal" data-target="#myModal">ADD NEW TASK</button>
         <hr>
         <!-- backlog -->
         <div class="col-md-3">
           <div class="row">
             <div class="col-sm-12">
-              <div class="thumbnail">
-                <h3>Backlog</h3>
-                <div class="thumbnail" v-for="backlog in backlogs" v-if="backlog.status == 'backlog'">
+              <div class="panel panel-danger">
+                <div class="panel panel-heading"><h3><strong>BACKLOG</strong></h3></div>
+                <div class="panel-body">
+                  <div class="thumbnail" v-for="backlog in backlogs" v-if="backlog.status == 'backlog'">
                   <div class="caption">
                     <div>
-                      <h5>{{backlog.title}}</h5>
+                      <h4><strong>{{backlog.title}}</strong></h4>
                       <hr>
                       <p>Point: {{backlog.point}}</p>
                       <p>Assigned To: {{backlog.assign}}</p>
-                      <button @click="showDetail(backlog)" class="btn btn-info" data-toggle="modal" data-target="#modalDetail">Detail</button>
+                      <button @click="showDetail(backlog)" class="btn btn-danger" data-toggle="modal" data-target="#modalDetail">Detail</button>
                     </div>
                   </div>
                   
                   <!-- modal detail -->
+                </div>
                 </div>
               </div>
             </div>
@@ -34,18 +36,20 @@
         <div class="col-md-3">
           <div class="row">
             <div class="col-sm-12">
-              <div class="thumbnail">
-                <h3>Todo</h3>
+              <div class="panel panel-warning">
+                <div class="panel panel-heading"><h3><strong>TO DO</strong></h3></div>
+                <div class="panel-body">
                 <div class="thumbnail" v-for="backlog in backlogs" v-if="backlog.status == 'todo'">
                   <div class="caption">
                     <div>
-                      <h5>{{backlog.title}}</h5>
+                      <h4><strong>{{backlog.title}}</strong></h4>
                       <hr>
                       <p>Point: {{backlog.point}}</p>
                       <p>Assigned To: {{backlog.assign}}</p>
-                      <button @click="showDetail(backlog)" class="btn btn-info" data-toggle="modal" data-target="#modalDetail">Detail</button>
+                      <button @click="showDetail(backlog)" class="btn btn-warning" data-toggle="modal" data-target="#modalDetail">Detail</button>
                     </div>
                   </div>
+                </div>                  
                 </div>
               </div>
             </div>
@@ -56,19 +60,21 @@
         <div class="col-md-3">
           <div class="row">
             <div class="col-sm-12">
-              <div class="thumbnail">
-                <h3>On Going</h3>
+              <div class="panel panel-success">
+                <div class="panel panel-heading"><h3><strong>ON GOING</strong></h3></div>
+                <div class="panel-body">
                 <div class="thumbnail" v-for="backlog in backlogs" v-if="backlog.status == 'doing'">
                   <div class="caption">
                     <div>
-                      <h5>{{backlog.title}}</h5>
+                      <h4><strong>{{backlog.title}}</strong></h4>
                       <hr>
                       <p>Point: {{backlog.point}}</p>
                       <p>Assigned To: {{backlog.assign}}</p>
-                      <button @click="showDetail(backlog)" class="btn btn-info" data-toggle="modal" data-target="#modalDetail">Detail</button>
+                      <button @click="showDetail(backlog)" class="btn btn-success" data-toggle="modal" data-target="#modalDetail">Detail</button>
                     </div>
                   </div>
                 </div>
+              </div>                
               </div>
             </div>
           </div>
@@ -78,12 +84,13 @@
         <div class="col-md-3">
           <div class="row">
             <div class="col-sm-12">
-              <div class="thumbnail">
-                <h3>Done</h3>
+              <div class="panel panel-info">
+                <div class="panel panel-heading"><h3><strong>DONE</strong></h3></div>
+                <div class="panel-body">
                 <div class="thumbnail" v-for="backlog in backlogs" v-if="backlog.status == 'done'">
                   <div class="caption">
                     <div>
-                      <h5>{{backlog.title}}</h5>
+                      <h4><strong>{{backlog.title}}</strong></h4>
                       <hr>
                       <p>Point: {{backlog.point}}</p>
                       <p>Assigned To: {{backlog.assign}}</p>
@@ -91,6 +98,7 @@
                     </div>
                   </div>
                 </div>
+              </div>
               </div>
             </div>
           </div>
@@ -112,19 +120,19 @@
             <form>
               <div class="form-group">
                 <label for="Title">Title: </label>
-                <input type="text" v-model="addTasks.title" class="form-control" id="title">
+                <input type="text" v-model="addTasks.title" class="form-control" id="title" placeholder="Title Task">
               </div>
               <div class="form-group">
                 <label for="Point">Point: </label>
-                <input type="text" v-model="addTasks.point" class="form-control" id="point">
+                <input type="text" v-model="addTasks.point" class="form-control" id="point" placeholder="0-100">
               </div>
               <div class="form-group">
                 <label for="Description">Description</label>
-                <textarea v-model="addTasks.description" class="form-control" rows="5" id="description"></textarea>
+                <textarea v-model="addTasks.description" class="form-control" rows="5" id="description" placeholder="Task Description"></textarea>
               </div>
               <div class="form-group">
                 <label for="Assign">Assigned To: </label>
-                <input type="text" v-model="addTasks.assign" class="form-control" id="assign">
+                <input type="text" v-model="addTasks.assign" class="form-control" id="assign" placeholder="People in Charge">
               </div>
             </form>
           </div>
@@ -143,12 +151,13 @@
         <div class="modal-content">
           <div class="modal-header">
             <button class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title">Detail Task: {{currentTask.title}} for {{currentTask.assign}}</h4>
+            <h4 class="modal-title">Detail Task</h4>
           </div>
           <div class="modal-body">
             <form>
               <div class="form-group">
-                <label for="Title">Task Description: {{currentTask.description}}</label>
+                <label for="Title">{{currentTask.title}} for {{currentTask.assign}}</label>
+                <p>Task Description: {{currentTask.description}}</p>
                 <p>Point: {{currentTask.point}}</p>
                 <p>Status: {{currentTask.status}}</p>
               </div>
@@ -225,7 +234,7 @@ export default {
     backlog (tasks) {
       let data = db.child(tasks['.key'])
       data.update({
-        'status': 'todo'
+        'status': 'backlog'
       })
     },
     todo (tasks) {
@@ -249,7 +258,6 @@ export default {
   }
 }
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h1, h2 {
